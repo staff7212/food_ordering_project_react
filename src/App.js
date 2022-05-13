caba0775e522c5e5b273163cf4d08273a2506ae4
Saveliy <staff7212@gmail.com>
2022-05-13 14:58:02 +0300

    meals list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
-import Header from './component/Layout/Header'
+import Header from './component/Layout/Header';
+import Meals from './component/Meals/Meals';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/component/Meals/Meals.js
@@ -0,0 +1,14 @@
+
+import PromoText from './PromoText';
+import MealList from './MealList'
+
+const Meals = () => {
+  return (
+    <>
+      <PromoText />
+      <MealList />
+    </>
+  )
+}
+
+export default Meals
